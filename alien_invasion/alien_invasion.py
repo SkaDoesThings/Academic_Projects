c96eeab233bd7a44dc00d62addcfe88f65e67b9f
SkaDoesThings <42884855+SkaDoesThings@users.noreply.github.com>
2025-03-19 23:19:46 -0400

    settings.py and ship.py
    - Settings can be stored
    - Ship attributes are streamlined

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,15 +2,21 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Manage assets and behavior"""
 
     def __init__(self):
         """Initialize game"""
         pygame.init()
+        self.settings = Settings()
     
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)        
         
     def run_game(self):
         """Game loop"""
@@ -18,6 +24,12 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+        
+            # Redraw screen during each loop pass
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+        
+            # Draw most recent screen
             pygame.display.flip()
         
 if __name__ == '__main__':
---
+++ alien_invasion/ship.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Ship:
+    """Class to manage the ship"""
+    
+    # Note: Rect stands for rectangle
+    def __init__(self, ai_game):
+        # Initilize ship and its starting position
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        # Load ship image
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        
+        # Start ship at the bottom center of the field
+        self.rect.midbottom = self.screen_rect.midbottom
+        
+    def blitme(self):
+        # Draw ship at its current location
+        self.screen.blit(self.image, self.rect)
+        
